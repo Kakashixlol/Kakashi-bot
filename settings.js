5c072a5ab85434e57843f3c0719dbf917138c14b
Kakashixlol <142254758+Kakashixlol@users.noreply.github.com>
2023-08-14 20:58:35 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -15,23 +15,23 @@ global.doc5 = 'application/pdf'
 global.doc6 = 'application/vnd.android.package-archive'
 
 //owmner v card
-global.owner = ['201098906252'] //ur owner number
-global.ownername = "ᗴᒪᘜᗩᘔᗩᖇ ᗩᒪᗯᗩᘔᗴᖇ❤️" //ur owner name
-global.ytname = "YT: ELGAZAR" //ur yt chanel name
-global.socialm = "GitHub: ELGAZAR" //ur github or insta name
+global.owner = ['201033401721'] //ur owner number
+global.ownername = "omar❤️" //ur owner name
+global.ytname = "kafrawy" //ur yt chanel name
+global.socialm = "GitHub:kakashixlol" //ur github or insta name
 global.location = "Africa, Egypt, Cairo" //ur location
 
 //bot bomdy 
-global.ownernomer = "201098906252" //ur number
-global.premium = ['201098906252'] //ur premium number
-global.botname = '❤️ᗴᒪᘜᗩᘔᗩᖇ ᗷOT❤️' //ur bot name
-global.linkz = "https://youtube.com/@ABDALLAH_MOHAMED" //your theme url which will be displayed on whatsapp
-global.websitex = "https://telegra.ph/file/ec763cc3e7c88d0e67682.jpg" //ur website to be displayed
-global.botscript = 'https://youtube.com/@ABDALLAH_MOHAMED' //script link
+global.ownernomer = "201033401721" //ur number
+global.premium = ['000'] //ur premium number
+global.botname = '❤️ KAKASHI ᗷOT❤️' //ur bot name
+global.linkz = "https://youtube.com/OMAR" //your theme url which will be displayed on whatsapp
+global.websitex = "https://" //ur website to be displayed
+global.botscript = 'https://LOL //script link
 global.themeemoji = "♥" //ur theme emoji
-global.packname = "⍣الجزار الوزير يروحي⍣" //ur sticker watermark packname
-global.author = "⍣01098906252⍣" //ur sticker watermark author
-global.wm = "❤️ᗴᒪᘜᗩᘔᗩᖇ ᗷOT❤️." //ur watermark
+global.packname = "⍣الحج كاكاشي⍣" //ur sticker watermark packname
+global.author = "⍣01033401721⍣" //ur sticker watermark author
+global.wm = "❤️KAKASHI BOT❤️." //ur watermark
 
 // Other
 global.sessionName = 'session'
